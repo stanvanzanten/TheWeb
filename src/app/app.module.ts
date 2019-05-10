89451d8a89cad54936b8a782c68812f8aa400675
Stan van Zanten <stanzanten@hotmail.com>
2019-05-10 10:06:38 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FactsComponent } from './components/facts/facts.component';
+
+
+const appRoutes : Routes =[
+  {path:'', component: HomeComponent},
+  {path:'home', component: HomeComponent},
+  {path:'facts', component: FactsComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FactsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
