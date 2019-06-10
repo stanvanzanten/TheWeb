89203eaec04d89cf9aaf9b99c075f28972bf7fa4
Stan van Zanten <stanzanten@hotmail.com>
2019-06-10 22:49:39 +0200

    Made some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   animations: [
-    // fader
+    // fader,
     slider
     // transformer,
     // stepper
